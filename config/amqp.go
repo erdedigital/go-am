c0973002f5b0825dcedefb56811a7fd6bae90bd9
DEDE YUYANDI <erdedigital@gmail.com>
2020-11-28 17:26:00 +0700

    first commit

---
+++ config/amqp.go
@@ -0,0 +1,13 @@
+package config
+
+import (
+	"github.com/streadway/amqp"
+)
+
+func  ConnAMQP() *amqp.Connection {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	if err != nil {
+		panic(err)
+	}
+	return conn
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/erdedigital/go-amqp
+
+go 1.13
+
+require github.com/streadway/amqp v1.0.0
